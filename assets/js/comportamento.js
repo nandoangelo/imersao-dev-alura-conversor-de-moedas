f567271915957cbe4f9dd7b21fea044ff6cc72e2
Fernando Angelo <nandoangelo@gmail.com>
2023-02-02 17:39:40 -0300

    Formulário
    Implementando um formulário para seleção da moeda que deseja converter em reais.

--- assets/js/comportamento.js
+++ assets/js/comportamento.js
@@ -3,38 +3,49 @@
  * do Real Brasileiro (R$) no dia 02/02/2023
  */
 
-const cotacaoDoDolar = 5.0307; // 5.03 reais
-const cotacaoDoEuro = 5.4496; // 5.45 reais
-const cotacaoDaLibra = 6.1350; // 6.13 reais
-
-let valorParaConverter = 0;
-let valorConvertido = 0;
-
-function estrangeiroParaReal (valorParaConverter, moedaEstrangeira) {
-  return valorParaConverter * moedaEstrangeira;
-}
-
-function realParaEstrangeiro (valorEmReal, moedaEstrangeira) {
-  return valorEmReal / moedaEstrangeira;
-}
-
-function estrangeiroParaEstrangeiro (moedaOrigem, moedaConverter) {
-  return valorParaConverter * (moedaOrigem / moedaConverter);
-}
+const cotacaoDoDolar = 5.0307; // 5,03 reais
+const cotacaoDoEuro = 5.4496; // 5,45 reais
+const cotacaoDaLibra = 6.1350; // 6,13 reais
+const cotacaoDoBitcoin = 120135.69; // 120.135,69 reais
 
 // Moedas:
 // 'BRL' = Real brasileiro (valor padrão)
 // 'USD' = Dólar americano
 // 'EUR' = Euro
 // 'GBP' = Libras esterlinas
+// 'BTC' = Bitcoins
 function formataValores(valor, moeda = 'BRL') {
   const valorFormatado = new Intl.NumberFormat('pt-BR', {style:'currency', currency: moeda}).format(valor);
   return valorFormatado;
 }
 
-valorParaConverter = prompt('Informe um valor em dólares (US$):')
-valorParaConverter = Number(valorParaConverter);
+function converterMoedas() {
+  let valorParaConverter = document.getElementById('valorParaConverter');
+  let moedaOrigem = document.getElementById('moedaOrigem');
+  let res = document.getElementById('resposta');
+  let valorConvertido = 0;
 
-valorConvertido = estrangeiroParaReal(valorParaConverter, cotacaoDoDolar);
+  valorParaConverter = Number(valorParaConverter.value);
 
-alert(`${formataValores(valorParaConverter, 'USD')} dólares é o mesmo que ${formataValores(valorConvertido)}`)
+  switch (moedaOrigem.value) {
+    case 'USD':
+      valorConvertido = valorParaConverter * cotacaoDoDolar;
+    break;
+    case 'EUR':
+      valorConvertido = valorParaConverter * cotacaoDoEuro;
+    break;
+    case 'GBP':
+      valorConvertido = valorParaConverter * cotacaoDaLibra;
+    break;
+    case 'BTC':
+      valorConvertido = valorParaConverter * cotacaoDoBitcoin;
+    break;
+    default:
+    alert('Desculpe. Não encontrei o valor que vc queria')
+  }
+
+  res.innerText = formataValores(valorConvertido);
+
+  console.log(valorConvertido);
+
+}
